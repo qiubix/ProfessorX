c4111ab1a46f7826ced806bcd6e411c2ded2b2d2
qiubix <qiubix@gmail.com>
2015-07-13 01:55:25 +0200

    Arabic 10 is Roman X

--- RomanConverterTest.cpp
+++ RomanConverterTest.cpp
@@ -3,9 +3,10 @@ using ::testing::Eq;
 
 std::string convert(unsigned int arabic) {
   std::string roman{""};
-  while(arabic-- > 0) {
+  if (arabic == 10)
+    return "X";
+  while(arabic-- > 0)
     roman += "I";
-  }
   return roman;
 }
 
@@ -13,4 +14,5 @@ TEST(RomanNumeralConverter, CanConvertPositiveDigits) {
   EXPECT_THAT(convert(1), Eq("I"));
   EXPECT_THAT(convert(2), Eq("II"));
   EXPECT_THAT(convert(3), Eq("III"));
+  EXPECT_THAT(convert(10), Eq("X"));
 }
