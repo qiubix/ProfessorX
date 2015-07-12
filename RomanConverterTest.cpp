d6542812089067e9ed318c61ee223c8eedf636e0
qiubix <qiubix@gmail.com>
2015-07-13 01:51:42 +0200

    Introduce loop adding roman ones to the result

--- RomanConverterTest.cpp
+++ RomanConverterTest.cpp
@@ -2,13 +2,15 @@
 using ::testing::Eq;
 
 std::string convert(unsigned int arabic) {
-  if (arabic == 2) {
-    return "II";
+  std::string roman{""};
+  while(arabic-- > 0) {
+    roman += "I";
   }
-  return "I";
+  return roman;
 }
 
 TEST(RomanNumeralConverter, CanConvertPositiveDigits) {
   EXPECT_THAT(convert(1), Eq("I"));
   EXPECT_THAT(convert(2), Eq("II"));
+  EXPECT_THAT(convert(3), Eq("III"));
 }
