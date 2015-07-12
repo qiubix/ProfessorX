39a71a59763fb8f27316259dbf20cfcec5c911e0
qiubix <qiubix@gmail.com>
2015-07-13 00:42:29 +0200

    Pass first test

--- RomanConverterTest.cpp
+++ RomanConverterTest.cpp
@@ -1,4 +1,9 @@
 #include "gmock/gmock.h"
+using ::testing::Eq;
+
+std::string convert(unsigned int arabic) {
+  return "I";
+}
 
 TEST(RomanNumeralConverter, CanConvertPositiveDigits) {
   EXPECT_THAT(convert(1), Eq("I"));
