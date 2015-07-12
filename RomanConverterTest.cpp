ff777c11b382f00f8da04471b56745fd18bdffa2
qiubix <qiubix@gmail.com>
2015-07-13 01:39:53 +0200

    Add new test case

--- RomanConverterTest.cpp
+++ RomanConverterTest.cpp
@@ -2,9 +2,13 @@
 using ::testing::Eq;
 
 std::string convert(unsigned int arabic) {
+  if (arabic == 2) {
+    return "II";
+  }
   return "I";
 }
 
 TEST(RomanNumeralConverter, CanConvertPositiveDigits) {
   EXPECT_THAT(convert(1), Eq("I"));
+  EXPECT_THAT(convert(2), Eq("II"));
 }
